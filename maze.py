fb29d2dd6ed773b106709a1a15d02b53a7ef90d4
mem_dixy <mem_dixy@pm.me>
2022-08-22 21:19:35 +0000

    Pylinted a bunch of stuff.

--- maze.py
+++ maze.py
@@ -1,3 +1,6 @@
+"""Generate a maze in Blender with 1000 dead ends."""
+import random
+
 cell_width = 80
 cell_height = 45
 
@@ -34,61 +37,73 @@ HORIZONTAL = 2
 
 
 def draw():
-    print("=====================")
+    """Draws the map in ascii characters."""
     index = 0
     for cell in maze:
-        value = str(hex(cell))[2:3]
-        if cell == WALL:
-            value = "@"
-        if cell == HOLE:
-            value = " "
-        if cell == 0:
-            value = " "
+        value = "@" if cell == WALL else " "
         print(value, end="")
         index += 1
         if index % width == 0:
+            # print newline at end of line
             print("")
 
 
-def go_north(cell):
-    value = cell - width - width
-    return value
+def north_cell(cell):
+    """Return the cell north of this cell."""
+    return cell - width - width
 
 
-def go_south(cell):
-    value = cell + width + width
-    return value
+def south_cell(cell):
+    """Return the cell south of this cell."""
+    return cell + width + width
 
 
-def go_east(cell):
-    value = cell + 1 + 1
-    return value
+def east_cell(cell):
+    """Return the cell east of this cell."""
+
+    return cell + 1 + 1
 
 
-def go_west(cell):
-    value = cell - 1 - 1
-    return value
+def west_cell(cell):
+    """Return the cell west of this cell."""
+    return cell - 1 - 1
 
 
-def int_from_cell(cell):
-    value = maze[cell]
-    return value
+def north_wall(cell):
+    """Return the wall value north of this cell."""
+    return maze[cell - width]
+
+
+def south_wall(cell):
+    """Return the wall value south of this cell."""
+    return maze[cell + width]
+
+
+def east_wall(cell):
+    """Return the wall value east of this cell."""
+    return maze[cell + 1]
+
+
+def west_wall(cell):
+    """Return the wall value west of this cell."""
+    return maze[cell - 1]
 
 
 def move(cell, direction):
-    match direction:
-        case 1:  # NORTH
-            pick = go_north(cell)
-        case 2:  # SOUTH
-            pick = go_south(cell)
-        case 4:  # EAST
-            pick = go_east(cell)
-        case 8:  # WEST
-            pick = go_west(cell)
-    return pick
+    """Move to a neighboring cell from the selected direction."""
+    if direction == NORTH:
+        neighbor = north_cell(cell)
+    if direction == SOUTH:
+        neighbor = south_cell(cell)
+    if direction == EAST:
+        neighbor = east_cell(cell)
+    if direction == WEST:
+        neighbor = west_cell(cell)
+    return neighbor
 
 
 def uncarved_neighbor(cell_start, direction):
+    """Tell our neighbors to not carve into us anymore."""
     cell = move(cell_start, direction)
     value = grid[cell]  # grid shows neighbors
 
@@ -102,24 +117,25 @@ def uncarved_neighbor(cell_start, direction):
         maze[cell] &= UNEAST
 
     if value & NORTH:
-        pick = go_north(cell)
+        pick = north_cell(cell)
         oppsite = UNSOUTH
         maze[pick] &= oppsite
     if value & SOUTH:
-        pick = go_south(cell)
+        pick = south_cell(cell)
         oppsite = UNNORTH
         maze[pick] &= oppsite
     if value & EAST:
-        pick = go_east(cell)
+        pick = east_cell(cell)
         oppsite = UNWEST
         maze[pick] &= oppsite
     if value & WEST:
-        pick = go_west(cell)
+        pick = west_cell(cell)
         oppsite = UNEAST
         maze[pick] &= oppsite
 
 
-def carve(cell, direction, insede=True):
+def carve(cell, direction, inside=True):
+    """Carve into a neighobring cell based off the direction."""
     pick = 0
     frog = cell
     oppsite = 0
@@ -133,13 +149,14 @@ def carve(cell, direction, insede=True):
         case 8:  # WEST
             pick = frog - 1
     maze[pick] = HOLE
-    if insede:
+    if inside:
         uncarved_neighbor(cell, direction)
     return move(cell, direction)
 
 
 def carve_random(cell):
-    direction = int_from_cell(cell)
+    """Pick a random direction to carve from the valid directions."""
+    direction = maze[cell]
     choice = []
     if direction & NORTH:
         choice.append(NORTH)
@@ -155,25 +172,24 @@ def carve_random(cell):
     return carve(cell, direction)
 
 
-import random
-
-
 def random_from_list():
+    """Return a random cell from the list of unfinished cells."""
     length = len(line)
     index = random.randrange(length)
     cell = line[index]
     return cell
 
 
-
 def count():
+    """Count all the walls in the current maze."""
     count = 0
+    # loop over each cell from a copy of cells from the maze
     for cell in copy:
         walls = 0
-        walls += 1 if maze[cell - width] == WALL else 0
-        walls += 1 if maze[cell + width] == WALL else 0
-        walls += 1 if maze[cell + 1] == WALL else 0
-        walls += 1 if maze[cell - 1] == WALL else 0
+        walls += 1 if north_wall(cell) == WALL else 0
+        walls += 1 if south_wall(cell) == WALL else 0
+        walls += 1 if east_wall(cell) == WALL else 0
+        walls += 1 if west_wall(cell) == WALL else 0
         count += 1 if walls == 3 else 0
     return count
 
@@ -214,11 +230,8 @@ def mazeit(seed):
             copy.append(cell)
             line.append(cell)
 
-    #enter = random.randrange(0, cell_width)
-    #exit = random.randrange(cell_size - cell_width, cell_size)
     enter = random.randrange(cell_height) * cell_width
     exit = random.randrange(cell_height) * cell_width + cell_width - 1
-
 
     enter = copy[enter]
     exit = copy[exit]
@@ -248,49 +261,54 @@ def mazeit(seed):
     return count()
 
 
-for seed in range(1000 * 0):
-    many = mazeit(seed)
-    if many == 1000:
-        print("HEEEERERERERER", seed)
-        draw()
-    #print(seed, many)
-
-print(mazeit(360))
-draw()
-
-
 def finish():
     final = []
     index = 0
     for cell in maze:
         if cell == WALL:
-            has_north = (index - width) >= 0
-            has_south = (index + width) < size
-            has_east = ((index + 1) % width) != 0
-            has_west = ((index + 0) % width) != 0
+            has_north_wall = (index - width) >= 0
+            has_south_wall = (index + width) < size
+            has_east_wall = ((index + 1) % width) != 0
+            has_west_wall = ((index + 0) % width) != 0
 
             direction = 0
-            if not has_north or maze[index - width] != WALL:
-                direction += 1
+            if not has_north_wall or north_wall(index) != WALL:
+                direction |= NORTH
 
-            if not has_south or maze[index + width] != WALL:
-                direction += 2
+            if not has_south_wall or south_wall(index) != WALL:
+                direction |= SOUTH
 
-            if not has_east or maze[index + 1] != WALL:
-                direction += 4
+            if not has_east_wall or east_wall(index) != WALL:
+                direction |= EAST
 
-            if not has_west or maze[index - 1] != WALL:
-                direction += 8
+            if not has_west_wall or west_wall(index) != WALL:
+                direction |= WEST
 
             final.append(direction)
         else:
-            final.append(-1)
+            final.append(HOLE)
         index += 1
         if index % width == 0:
+            # end of line marker
             final.append(None)
     return final
 
 
-final = finish()
-print(final)
+def find_seed():
+    """Look through 1000 mazes to find a maze with 1000 dead ends."""
+    for seed in range(1000):
+        many = mazeit(seed)
+        if many == 1000:
+            print(seed)
+            draw()
+    print("End of seed search.""")
+
+
+# find_seed()
+
+print(mazeit(360))
+draw()
+
+maze = finish()
+print(maze)
 
