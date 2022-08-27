5207d0b92629a97d95ccb5a1f7b11871a1cc3757
mem_dixy <mem_dixy@pm.me>
2022-08-27 04:38:48 +0000

    Step towards dynamic photo loading.

--- celestine/window/main.py
+++ celestine/window/main.py
@@ -1,21 +1,62 @@
 import celestine.core.load as load
+import celestine.core.os as os
 
-image = {}
+#image = {}
+image = []
+
+#blender only
+Image_Formats = [
+    ".bmp",
+    ".sgi",
+    ".rgb",
+    ".bw",
+    ".png",
+    ".jpg",
+    ".jpeg",
+    ".jp2",
+    ".j2c",
+    ".tga",
+    ".cin",
+    ".dpx",
+    ".exr",
+    ".hdr",
+    ".tif",
+    ".tiff",
+    ".webp",
+]
+
+def execute(directory):
+    (path, file) = os.walk_directory(directory)
+    images = []
+    for (filenames) in file:
+        (dirpath, name) = filenames
+        ext = os.file_extension(name).lower()
+        if ext in Image_Formats:
+            merge = os.join(dirpath, name)
+            images.append(merge)
+    return images
+
+
 
 def setup(session):
     global image
     window = session.task
     directory =  session.directory
-    image1 = load.path("D:\\", "file", "test.jpg")
-    image2 = load.path("D:\\", "file", "test2.jpg")
-    image["image1"] = window.image_load(image1)
-    image["image2"] = window.image_load(image2)
+    
+    directory = "D:\\file\\"
+    images = execute(directory)
+    for imaged in images:
+        image.append(window.image_load(imaged))
 
 
 def view(session):
     global image
     window = session.task
-    window.image("00", image["image1"])
-    window.image("01", image["image2"])
+    for imaged in image:
+        window.image("00", imaged)
+
     window.label("Settings", "no puppy. File Explorer using Tkinter")
     window.file_dialog("set", "Settings")
+
+
+
