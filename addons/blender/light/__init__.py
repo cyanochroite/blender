79875355b2b1b97b34f34cb6e65e693e9ec427f8
mem_dixy <mem_dixy@pm.me>
2022-08-28 14:27:32 +0000

    Inh

--- addons/blender/light/__init__.py
+++ addons/blender/light/__init__.py
@@ -1,9 +1,35 @@
 # <pep8-80 compliant>
 """Light data-block for lighting a scene."""
-from blender import data
+import bpy
 
 
-import blender.light.area
-import blender.light.point
-import blender.light.spot
-import blender.light.sun
+class all():
+    @staticmethod
+    def new(data, *args):
+        print("eat")
+        return data.new(*args)
+
+    @staticmethod
+    def object_(name, object_data):
+        object = bpy.data.objects.new(name, object_data)
+        bpy.context.scene.collection.objects.link(object)
+        return object
+
+
+class lighter(all):
+    @classmethod
+    def new(cls, *args):
+        print("you")
+        return super().new(bpy.data.lights, *args)
+
+    @classmethod
+    def make(cls, name):
+        print("hi")
+        return cls.object_(name, cls.new(name))
+
+
+class sun(lighter):
+    @classmethod
+    def new(cls, name):
+        print("cow")
+        return super().new(name, 'SUN')
