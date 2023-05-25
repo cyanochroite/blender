2708864930264f1f4640b291122a76ff6892c01c
mem_dixy <mem_dixy@pm.me>
2023-05-25 01:34:00 +0000

    Path traversal avoidance code notes.

--- addons/celestine/__main__.py
+++ addons/celestine/__main__.py
@@ -6,3 +6,22 @@ sys.path[0] = os.path.dirname(sys.path[0])
 
 celestine = __import__("celestine")
 celestine.main(sys.argv[1:], True)
+
+
+
+def logo():
+    base = "/todoapp/content/images"
+    img = request.args.get('image_name')
+    safepath = os.path.realpath(img)
+    prefix = os.path.commonpath(base, safepath)
+
+    if prefix == base:
+      return send_file(os.path.join(base, safepath))
+
+
+  f = request.files['file']
+  filename = secure_filename(f.filename)
+
+
+def allowed_file(filename):
+    return os.path.splitext(filename)[1] in app.config["ALLOWED_EXTENSIONS"]
